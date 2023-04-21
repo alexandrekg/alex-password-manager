74585b815ca89db03e5abe4bf5f4a24ecf2864b5
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-04-21 14:39:27 -0300

    update: src

--- src/retrieve.py
+++ src/retrieve.py
@@ -1,6 +1,31 @@
 import click
+from pathlib import Path
+from conf import PASS_URL
 
 
 @click.command()
-def retrieve():
-    print('Get data')
+@click.option('--service', default="", help="If you want to get a specific data")
+def retrieve(service):
+    if service:
+        print('Get specified data')
+    else:
+        data = _get_json_data()
+        print(data)
+
+
+def _get_json_data():
+    retrieved_data = []
+    if not _validate_file_path():
+        print("Can't acess requested data.")
+        return
+
+    with open(PASS_URL, "r") as data:
+        for d in data:
+            retrieved_data.append(d)
+
+    return retrieved_data
+
+def _validate_file_path():
+    expected_file_path = Path(PASS_URL)
+
+    return expected_file_path.is_file()
--- src/save.py
+++ src/save.py
@@ -19,9 +19,3 @@ def save(service, account, password):
         print('Saving data on file.')
         pass_file.write(data_to_save)
         print('Done.')
-
-
-def _validate_file_path():
-    expected_file_path = Path(PASS_URL)
-
-    return expected_file_path.is_file()
