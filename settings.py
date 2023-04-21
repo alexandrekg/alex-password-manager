d052e65b1fa12c822d7b76c90ba1faeccfaf36c2
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-04-21 14:45:43 -0300

    update settings with MONGO data

--- settings.py
+++ settings.py
@@ -1,3 +1,9 @@
 FILE_NAME = 'pass.json'
 PASS_URL = f"C:\\Users\\alexa\\Documents\\workspace\\alex-password-manager\\{FILE_NAME}"
-MONGO_URI = "mongodb+srv://localhost"
+
+MONGO = {
+    "mongo_uri": "mongodb+srv://localhost",
+    "database": "xx",
+    "user": "xxx",
+    "password": "xxx"
+}
