998aa91ec5316333a02165473d9131c8c0bf619f
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-04-21 13:22:30 -0300

    creating conf.py to save pass_file path

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import click
+from pathlib import Path
+
+from conf import PASS_URL
+
+
+@click.option('--service_name',  help='Save password from ... (Ex: Facebook, Instagram, Github, etc.)')
+@click.option('--account', help='Your username, account, e-mail, etc...')
+@click.option('--password', help='Your password')
+def run(service_name, account, password):
+    print(f'Your {service_name} data: {account} / {password}')
+
 
 @click.command()
-@click.option('--service_name', help='Save password from ... (Ex: Facebook, Instagram, Github, etc.)')
-@click.option('--add', help='')
-@click.option('--pass', help='Your passwor')
-def run():
-    print('Run!')
+@click.option('--action')
+def cli(action):
+    if action == 'get':
+        get_data()
+    elif action == 'save':
+        save_data()
+
+def get_data():
+    pass
+
+def save_data():
+    pass
 
 
 if __name__ == "__main__":
-    run()
+    cli()
