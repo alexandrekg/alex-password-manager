9ab4dbd6dda50caa05ebef93970e149b2516c972
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-04-22 11:35:07 -0300

    update: removing MONGO_USER and MONGO_PASS from settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,4 @@
 FILE_NAME = 'pass.json'
 PASS_URL = f"C:\\Users\\alexa\\Documents\\workspace\\alex-password-manager\\{FILE_NAME}"
 
-MONGO_USER = "xxx"
-MONGO_PASS = "xxx"
 MONGO_URI = "mongodb+srv://localhost"
