fc2021af885513fa8f7705bed937e60ec0b09f9a
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-04-25 12:46:49 -0300

    add new features

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import click
-from src import save, retrieve
+from src import save, retrieve, create_person
 
 
 @click.group(help='CLI tool to manage my passwords')
 def cli():
     pass
 
-
+cli.add_command(create_person.create_person)
 cli.add_command(save.save)
 cli.add_command(retrieve.retrieve, name='get')
 
--- src/retrieve.py
+++ src/retrieve.py
@@ -1,7 +1,10 @@
 import click
+import getpass
 from conf import mongo_conn
 import subprocess
+import bcrypt
 
+conn = mongo_conn()
 
 @click.command()
 @click.option('--service', default="", help="If you want to get a specific data")
@@ -11,28 +14,40 @@ def retrieve(service, account):
         print('Please specify a service.')
         return
 
+    master_password = str(getpass.getpass('Your master password...  '))
+    if not master_password:
+        return
+
+    mongo_person = conn.find_one()
+
+    if not bcrypt.checkpw(master_password.encode('utf-8'), mongo_person['master_password']):
+        print('Passwords do not match!')
+        return
+
     print('Retrieving data...')
     data = get_data_from_mango(service)
-    print('Data retrieved')
     if data:
+        format_service_str = str(service).lower()
+        print('Data retrieved')
         if len(data) == 1:
-            return copy_pass(data[service][0]['password'])
+            return copy_pass(data['accounts'][0]['password'])
 
         if len(data) > 1 and not account:
             print('Please specify an account to get password')
             return
 
-        return copy_pass(data[service])
+        return copy_pass(data[format_service_str])
+    else:
+        print('Data has not been found for this service.')
 
 
 def get_data_from_mango(service):
-    filter = {}
+    if not service:
+        print('Please, specify a service name to copy password')
+        return
 
-    if service:
-        filter = {service: {'$exists': True}}
-
-    data = mongo_conn().find_one(filter, {'_id': 0})
-    return data
+    filter_query = {'service': str(service).lower()}
+    return mongo_conn().find_one(filter_query, {'_id': 0})
 
 
 def copy_pass(password):
