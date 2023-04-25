8c346361a845d99f45cf5a711f77bd19c000374b
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-04-25 12:39:04 -0300

    feat: add create_person function

---
+++ src/create_person.py
@@ -0,0 +1,21 @@
+from conf import mongo_conn
+import click
+import bcrypt
+
+conn = mongo_conn()
+
+
+@click.command()
+@click.option('--password', help="Master password")
+def create_person(password):
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    person_dict = {
+        'master_password': hashed_password
+    }
+    save(person_dict)
+
+
+def save(person):
+    print('Creating person data.')
+    conn.insert_one(person)
+    print('Person data has been created!')
